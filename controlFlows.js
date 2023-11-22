cde0cc2bae5f80c82bfd014627982c8d7bea4dc3
Svitlana Revniuk <69059791+Revniuk@users.noreply.github.com>
2023-11-22 11:55:48 -0500

    Add files via upload

---
+++ controlFlows.js
@@ -0,0 +1,35 @@
+// let num 
+// if (num > 0) {
+//     console.log("Yes")
+// } else {
+//     console.log("Also, yes")
+// }
+// let ageOfUser = 18
+// let userOutfit = "black"
+// if (ageOfUser >= 18 && userOutfit =="green") {
+//     console.log("You cam enter")
+// } else {
+//     console.log ("Access denied")
+// }
+let num = 100;
+if (num >= 0 && num > 100) {
+    console.log("Both conditions are TRUE");
+} else if (num >= 0 && num < 100) {
+    console.log("The number is positive and less than 100");
+} else {
+    console.log("The number is negative");
+}
+
+let value = 87;
+if (value >= 90) {
+    console.log("A");
+} else if (value >= 80) {
+    console.log("B");
+} else if (value >= 70) {
+    console.log("C");
+} else if (value >= 55) {
+    console.log("D");
+} else {
+    console.log("F");
+}
+
